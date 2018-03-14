8a2349c99bfa9441a2c8e50df51fc71012a0e158
Kjell Breiland <kjell.breiland@gmail.com>
2018-03-13 20:29:53 -0700

    remove old commented out stuff and old flags

--- main.go
+++ main.go
@@ -1,32 +1,14 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log"
 
 	"github.com/kjbreil/wsrcon"
 )
 
-var addr = flag.String("addr", "127.0.0.1:28016", "http service address")
-
 func main() {
-	flag.Parse()
-	log.SetFlags(0)
 
 	ss := wsrcon.Settings{Host: "127.0.0.1", Port: 28016, Password: "docker"}
-
-	// send := make(chan string)
-
-	// chat := make(chan string)
-
-	// generic := make(chan string)
-
-	// go func() {
-	// 	send <- "{\n  \"Identifier\": -1,\n  \"Message\": \"say HELLO\",\n  \"Name\": \"WebRcon\"}"
-	// }()
-
-	// ConnectOld(ss, send, generic, chat)
 
 	rcon := wsrcon.Connect(&ss)
 
